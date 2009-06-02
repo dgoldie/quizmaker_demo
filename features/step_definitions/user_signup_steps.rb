d7d3558f50dbc02aa70c0894222c74fe1c68527f
Doug Goldie <dgoldie15@gmail.com>
2009-06-02 10:54:27 -0700

    added initial bdd test

---
+++ features/step_definitions/user_signup_steps.rb
@@ -0,0 +1,16 @@
+
+Given /^an anonymous user$/ do
+  pending
+end
+
+When /^he signs up$/ do
+  pending
+end
+
+Then /^he has a user account$/ do
+  pending
+end
+
+Then /^gets a welcome message$/ do
+
+end
